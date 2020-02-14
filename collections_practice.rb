557a31062715845d19571c0d297a34b39431a8f7
Nicholas Patel <np4795@yahoo.com>
2020-02-14 17:54:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,88 +1,23 @@
-def sort_array_asc(array)
-  array.sort
-end
-
-sort_array_asc([25, 7, 1]) #[1,7,25]
-
-def sort_array_desc(array)
-  array.sort do | left, right|
-    right <=> left
-  end
-end
-
-sort_array_desc([25, 7, 14]) #[25, 14, 7]
-
-def sort_array_char_count(array)
-  array.sort do |left, right|
-    left.length <=> right.length
-  end
-end
-
-sort_array_char_count(["dogs", "cat", "Horses"]) #["cat", "dogs", "Horses"]
-
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
+  array[1..2] = array[1..2].sort do |a,b|
+    if a==b || a<b || a>b 
+      1
+    end
+  end
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
+test_array = [1,2,3,4]
+string_test = ["blake", "ashley", "scott"]
+#swap_elements(test_array)
 
-def reverse_array(array)
-  array.reverse
+def swap_elements_from_to(array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+  return array
 end
 
-reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
-
-def kesha_maker(array)
-  array.each do |item|
-    item[2] = "$"
+describe 'swap_elements_from_to' do
+  it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
+    expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
   end
 end
-
-kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
-
-def find_a(array)
-  array.find_all do |word|
-    word[0] == "a"
-  end
-
-  # using select method
-    # array.select do |word|
-    #   word[0] == "a"
-    # end
-end
-
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) #["apple", "avis", "arlo", "ascot"]
-
-def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum+=num
-  end
-  sum
-end 
-  # using reduce method
-    # array.reduce(:+)
-
-  # using inject method (short)
-     # array.inject(:+)
-
-  # using inject method (long)
-     # array.inject do |sum,x|
-     #  sum + x
-     # end
-end
-
-sum_array([11,4,7,8,9,100,134]) #273
-
-def add_s(array)
-  array.collect do |word|
-    if array[1] == word
-      word
-    else
-      word + "s"
-    end
-  end
-end
-
-add_s(["hand","feet", "knee", "table"]) #["hands","feet", "knees", "tables"]
